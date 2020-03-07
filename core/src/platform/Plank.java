682e7b6871f4c29328b64f08e23c7710adefad44
Vivi Ye <viviyyw0418@gmail.com>
2020-03-07 14:44:22 -0500

    some not working stuffs

--- core/src/platform/Plank.java
+++ core/src/platform/Plank.java
@@ -1,13 +1,14 @@
 package platform;
 
 import obstacle.BoxObstacle;
+import obstacle.WheelObstacle;
 
-public class Plank extends BoxObstacle {
+public class Plank extends WheelObstacle {
     public static final String PLANK_NAME = "plank";
     private int id;
 
-    public Plank(float x, float y, float width, float height, int id) {
-        super(x, y, width, height);
+    public Plank(float x, float y, float r, int id) {
+        super(x, y, r);
         setName(PLANK_NAME);
         this.id = id;
     }
