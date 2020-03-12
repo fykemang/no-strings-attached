ec368df36ce515fe6b18a41918156ecd3ecc30f3
ak778 <ak778@cornell.edu>
2020-03-12 15:27:09 -0400

    fixed level editor

--- LevelEditor/src/App.js
+++ LevelEditor/src/App.js
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home.js';
-import { Layout } from './componets/Layout';
-import { NavigationBar } from './componets/NavigationBar';
+import { Layout } from './components/Layout';
+import { NavigationBar } from './components/NavigationBar';
 // const { ReactDraggable: Draggable } = window;
-=======
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./Home.js";
-import { Layout } from "./components/Layout";
-import { NavigationBar } from "./components/NavigationBar";
->>>>>>> af5e5c31623add54e6f1ff309dc141799f186e91
 
 
 class App extends React.Component {
